b965bd2b7c7fa81856cfc3b70aec0f89cf816296
opsnoober <kuilong.liu@139.com>
2016-08-29 19:35:38 +0800

    完成install_depends中install pip功能

--- InstallAQCP/handler/install_depends.py
+++ InstallAQCP/handler/install_depends.py
@@ -2,6 +2,7 @@ import sys
 import os
 import shutil
 import subprocess
+import time
 sys.path.append('../')
 from utils.add_log import add_log
 
@@ -34,6 +35,7 @@ apt_deps_list = [
 'xfonts-intl-chinese-big'
 ]
 extra_deps_list = ['drozer_2.3.3.deb']
+pip_conf = os.path.join(os.path.dirname(basedir),'settings','pip.conf')
 
 def install_apt_deps():
     for apt_dep_pkg in apt_deps_list:
@@ -42,7 +44,19 @@ def install_apt_deps():
         subprocess.call(['dpkg','-i',os.path.join(os.path.dirname(basedir),'libs','extra',e_dep_pkg)])
     
 def install_pip_deps():
-    pass
+    try:
+        os.makedirs('/root/.pip')
+    except:
+        pass
+    shutil.copy(pip_conf,'/root/.pip/pip.conf')
+#############install pypiserver
+    subprocess.call(['pip','install',os.path.join(os.path.dirname(basedir),'libs','pips','pypiserver-1.1.10-py2.py3-none-any.whl')])
+############# run pypiserver
+    subprocess.Popen(['pypi-server','-p','7001',os.path.join(os.path.dirname(basedir),'libs','pips')])
+    time.sleep(3)
+##############install pip pkgs
+    subprocess.call(['pip','install','-r',os.path.join(os.path.dirname(basedir),'settings','requirement.txt'),'-i','http://127.0.0.1:7001/simple/'])
+    
 
 @add_log(__name__)
 def run():
