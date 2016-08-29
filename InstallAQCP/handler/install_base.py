ef7a66721a669eb101fbb580a2c76cf9c6b4e5a9
opsnoober <kuilong.liu@139.com>
2016-08-29 17:26:39 +0800

    完成install_base功能

--- InstallAQCP/handler/install_base.py
+++ InstallAQCP/handler/install_base.py
@@ -1,3 +1,26 @@
-def run():
-    print "install_base+++++++++++++++++++"
+import sys
+import os
+import shutil
+import subprocess
+sys.path.append('../')
+from utils.add_log import add_log
 
+basedir = os.path.split(os.path.realpath(__file__))[0] 
+lockfile = os.path.join(os.path.dirname(basedir),'lock','install_depends.lck')
+base_list = ['gcc','g++','make','zip','unzip','lrzsz','python-pip','python-dev']
+
+def install_base():
+    for base_pkg in base_list:
+        subprocess.call(['apt-get','-y','install',base_pkg])
+    
+
+@add_log(__name__)
+def run():
+    if os.path.exists(lockfile):
+        print "You can only run this part once time"
+        sys.exit(1)
+    else:
+        install_base()
+
+if __name__ == "__main__":
+    run()
--- InstallAQCP/handler/install_depends.py
+++ InstallAQCP/handler/install_depends.py
@@ -1,3 +1,29 @@
 def run():
     print "install_depends+++++++++++++++++++"
 
+import sys
+import os
+import shutil
+import subprocess
+sys.path.append('../')
+from utils.add_log import add_log
+
+basedir = os.path.split(os.path.realpath(__file__))[0] 
+lockfile = os.path.join(os.path.dirname(basedir),'lock','install_depends.lck')
+deps_list = ['gcc','g++','make','zip','unzip','lrzsz','python-pip','python-dev']
+
+def install_deps():
+    for dep_pkg in deps_list:
+        subprocess.call(['apt-get','-y','install',dep_pkg])
+    
+
+@add_log(__name__)
+def run():
+    if os.path.exists(lockfile):
+        print "You can only run this part once time"
+        sys.exit(1)
+    else:
+        install_deps()
+
+if __name__ == "__main__":
+    run()
