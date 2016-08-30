a2783d38caa70e1833c2ecc0e7710840cbf960d6
opsnoober <kuilong.liu@139.com>
2016-08-30 17:58:25 +0800

    fix bug

--- InstallAQCP/handler/install_depends.py
+++ InstallAQCP/handler/install_depends.py
@@ -4,10 +4,12 @@ import shutil
 import subprocess
 import time
 sys.path.append('../')
-from utils.add_log import add_log
+from utils.log import Logger
 
 basedir = os.path.split(os.path.realpath(__file__))[0] 
 lockfile = os.path.join(os.path.dirname(basedir),'lock','install_depends.lck')
+logger = Logger(logname=os.path.join(os.path.dirname(basedir),'logs','install.log'),loglevel=
+1,logger='install_depends').getlog()
 apt_deps_list = [
 'lib32z1',
 'lib32ncurses5',
@@ -34,15 +36,15 @@ apt_deps_list = [
 'ttf-wqy-microhei',
 'xfonts-intl-chinese-big'
 ]
-extra_deps_list = ['drozer_2.3.3.deb']
 pip_conf = os.path.join(os.path.dirname(basedir),'settings','pip.conf')
 
 def install_apt_deps():
     for apt_dep_pkg in apt_deps_list:
-        subprocess.call(['apt-get','-y','install',apt_dep_pkg])
-    for e_dep_pkg in extra_deps_list:
-        subprocess.call(['dpkg','-i',os.path.join(os.path.dirname(basedir),'libs','extra',e_dep_pkg)])
-    
+        child = subprocess.Popen(['apt-get','-y','install',apt_dep_pkg],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        (out,err) = child.communicate()
+        logger.info(out)
+        if err:
+            logger.error(err)
 def install_pip_deps():
     try:
         os.makedirs('/root/.pip')
@@ -50,23 +52,34 @@ def install_pip_deps():
         pass
     shutil.copy(pip_conf,'/root/.pip/pip.conf')
 #############install pypiserver
-    subprocess.call(['pip','install',os.path.join(os.path.dirname(basedir),'libs','pips','pypiserver-1.1.10-py2.py3-none-any.whl')])
+    child1 = subprocess.Popen(['pip','install',os.path.join(os.path.dirname(basedir),'libs','pips','pypiserver-1.1.10-py2.py3-none-any.whl')],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    (out1,err1) = child1.communicate()
+    logger.info(out1)
+    if err1:
+        logger.error(err1)
 ############# run pypiserver
     subprocess.Popen(['pypi-server','-p','7001',os.path.join(os.path.dirname(basedir),'libs','pips')])
-    time.sleep(3)
+   #time.sleep(3)
 ##############install pip pkgs
-    subprocess.call(['pip','install','-r',os.path.join(os.path.dirname(basedir),'settings','requirement.txt'),'-i','http://127.0.0.1:7001/simple/'])
+    logger.info('-------------------start install pip pkgs--------------------------------')
+    child2 = subprocess.Popen(['pip','install','-r',os.path.join(os.path.dirname(basedir),'settings','requirement.txt'),'-i','http://127.0.0.1:7001/simple/'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    (out2,err2) = child2.communicate()
+    logger.info(out2)
+    if err2:
+        logger.error(err2)
     
+    logger.info('------------------finish install pip pkgs--------------------------------')
 
-@add_log(__name__)
 def run():
     if os.path.exists(lockfile):
         print "You can only run this part once time"
         sys.exit(1)
     else:
+        logger.info('++++++++++++++ start install_depends ++++++++++++++++++')
         install_apt_deps()
         install_pip_deps()
         os.mknod(lockfile)
+        logger.info('++++++++++++++ end install_depends ++++++++++++++++++')
 
 if __name__ == "__main__":
     run()
